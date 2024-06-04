0a2814ce55d939938f8eccd1e8d6e97e4f75212c
Juan Ramiro Castro <juancastro@itba.edu.ar>
2024-06-04 19:06:33 -0300

    feat: Add reducer for query4 to compare with timings without combiner.

---
+++ api/src/main/java/ar/edu/itba/pod/query4/PlatesMostInfractionsByCountyNoCombinerReducerFactory.java
@@ -0,0 +1,38 @@
+package ar.edu.itba.pod.query4;
+
+import ar.edu.itba.pod.models.StringLongPair;
+import com.hazelcast.mapreduce.Reducer;
+import com.hazelcast.mapreduce.ReducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlatesMostInfractionsByCountyNoCombinerReducerFactory implements ReducerFactory<String, String, StringLongPair> {
+    @Override
+    public Reducer<String, StringLongPair> newReducer(String s) {
+        return null;
+    }
+
+    private static class PlatesMostInfractionsByCountyReducer extends Reducer<String, StringLongPair> {
+        private static final Long ZERO = 0L;
+        private final Map<String, Long> map = new HashMap<>();
+
+        @Override
+        public void reduce(String s) {
+            map.put(s, map.getOrDefault(s, ZERO) + 1);
+        }
+
+        @Override
+        public StringLongPair finalizeReduce() {
+            long max = 0;
+            String maxPlate = null;
+            for (Map.Entry<String, Long> entry : map.entrySet()) {
+                if (entry.getValue() > max) {
+                    max = entry.getValue();
+                    maxPlate = entry.getKey();
+                }
+            }
+            return StringLongPair.of(maxPlate, max);
+        }
+    }
+}
