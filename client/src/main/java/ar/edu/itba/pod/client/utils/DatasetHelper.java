6c54a9842d7fb654efe91828c45501d9368273fe
MSGronda <mgronda@itba.edu.ar>
2024-06-13 08:55:29 -0300

    feat: completed batched dataset loading

--- client/src/main/java/ar/edu/itba/pod/client/utils/DatasetHelper.java
+++ client/src/main/java/ar/edu/itba/pod/client/utils/DatasetHelper.java
@@ -6,144 +6,114 @@ import ar.edu.itba.pod.models.InfractionNYC;
 import ar.edu.itba.pod.models.abstractClasses.Infraction;
 import ar.edu.itba.pod.models.abstractClasses.Ticket;
 import com.hazelcast.core.IMap;
-import com.hazelcast.core.MultiMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public class DatasetHelper {
+    private static final Logger logger = LoggerFactory.getLogger(DatasetHelper.class);
     private static final int SKIP_CSV_LINES = 1;
     private static final int NUM_FIELDS_TICKET = 6;
     private static final int NUM_FIELDS_INFRACTION = 2;
-    private static final Logger logger = LoggerFactory.getLogger(DatasetHelper.class);
+    private static final int BATCH_SIZE = 100000;
+    public static final DateTimeFormatter nycFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    public static final DateTimeFormatter chiFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public static void loadNYCData(
+    public static void loadTicketBatched(
             Path infractionsPath,
             IMap<String, Infraction> infractions,
-            CsvReaderType infractionsReaderType,
+            InfractionCreator infractionCreator,
             Path ticketsPath,
             IMap<Long, Ticket> tickets,
-            CsvReaderType ticketReaderType,
-            Query query
+            Query query,
+            TicketCreator ticketCreator,
+            DateTimeFormatter dateFormatter
     ){
-        infractionsReaderType.reader.readCsv(infractionsPath, line -> {
+
+        Map<String, Infraction> localInfractions = new HashMap<>();       // Lo usamos para filtrar los tickets que no tienen infraction asociado
+
+        // Cargamos los infractions
+        sequentialReader.readCsv(infractionsPath, line -> {
             String[] fields = line.split(";");
 
             if(fields.length != NUM_FIELDS_INFRACTION){ return; }
 
-            infractions.put(
-                fields[0],
-                new InfractionNYC(Integer.parseInt(fields[0]), fields[1])
-            );
+            localInfractions.put(fields[0], infractionCreator.createInfraction(fields));
         });
 
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        // Mandamos todos al cluster
+        infractions.putAll(localInfractions);
 
         HashMap<Long, Ticket> batch = new HashMap<>();
         AtomicLong id = new AtomicLong(0);
 
-        ticketReaderType.reader.readCsv(ticketsPath, line -> {
+        sequentialReader.readCsv(ticketsPath, line -> {
 
             String[] fields = line.split(";");
 
-            if(fields.length != NUM_FIELDS_TICKET){ return; }
-
-            LocalDate date = LocalDate.parse(fields[1], dateFormatter);
+            // Si la linea del csv esta mal o no tiene un infractionCode valido
+            if (fields.length != NUM_FIELDS_TICKET || !localInfractions.containsKey(fields[2])) {
+                return;
+            }
 
             batch.put(
                     id.getAndIncrement(),
-                    query.getNYCTicket(
-                        fields[0],
-                        date,
-                        Integer.parseInt(fields[2]),
-                        Float.parseFloat(fields[3]),
-                        fields[4],
-                        fields[5]
-                    )
+                    ticketCreator.createTicket(fields, query, dateFormatter)
             );
 
-            if(batch.size() == 10000){
+            // Enviamos el batch de tickets al cluster
+            if (batch.size() == BATCH_SIZE) {
                 tickets.putAll(batch);
                 batch.clear();
             }
         });
+
     }
 
-    public static void loadCHIData(
-            Path infractionsPath,
-            IMap<String, Infraction> infractions,
-            CsvReaderType infractionsReaderType,
-            Path ticketsPath,
-            IMap<Long, Ticket> tickets,
-            CsvReaderType ticketReaderType,
-            Query query
-    ){
-        infractionsReaderType.reader.readCsv(infractionsPath, line -> {
-            String[] fields = line.split(";");
-
-            if(fields.length != NUM_FIELDS_INFRACTION){ return; }
-
-            infractions.put(
-                    fields[0],
-                    new InfractionCHI(fields[0], fields[1])
-            );
-        });
-
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-        ticketReaderType.reader.readCsv(ticketsPath, line -> {
-            String[] fields = line.split(";");
-
-            if(fields.length != NUM_FIELDS_TICKET){ return; }
-
-            LocalDateTime date = LocalDateTime.parse(fields[0], dateFormatter);
-            tickets.put(
-                    date.toEpochSecond(ZoneOffset.UTC),
-                    query.getCHITicket(
-                        date,
-                        fields[1],
-                        fields[2],
-                        fields[3],
-                        Integer.parseInt(fields[4]),
-                        fields[5]
-                    )
-            );
-        });
+    public interface InfractionCreator{
+        Infraction createInfraction(String[] fields);
     }
+    public static final InfractionCreator nycInfractionCreator = (fields) -> new InfractionNYC(Integer.parseInt(fields[0]), fields[1]);
+    public static final InfractionCreator chiInfractionCreator = (fields) -> new InfractionCHI(fields[0], fields[1]);
 
-    public enum CsvReaderType {
-        PARALLEL(parallelReader),
-        SEQUENTIAL(sequentialReader);
 
-        CsvReaderType(CsvReader reader){
-            this.reader = reader;
-        }
-        private final CsvReader reader;
+    public interface TicketCreator{
+        Ticket createTicket(String[] fields, Query query, DateTimeFormatter dateFormatter);
     }
+    public static final TicketCreator nycTicketCreator = (fields, query, dateFormatter) ->
+            query.getNYCTicket(
+                fields[0],
+                LocalDate.parse(fields[1], dateFormatter),
+                Integer.parseInt(fields[2]),
+                Float.parseFloat(fields[3]),
+                fields[4],
+                fields[5]
+    );
+
+    public static final TicketCreator chiTicketCreator = (fields, query, dateFormatter) ->
+            query.getCHITicket(
+                    LocalDateTime.parse(fields[0], dateFormatter),
+                    fields[1],
+                    fields[2],
+                    fields[3],
+                    Integer.parseInt(fields[4]),
+                    fields[5]
+    );
+
     private interface CsvReader {
         void readCsv(Path filepath, Consumer<String> consumer);
     }
-
-    private static final CsvReader parallelReader = (filepath, consumer) -> {
-        try (Stream<String> reader = Files.lines(filepath)) {
-            reader.skip(SKIP_CSV_LINES).parallel().forEach(consumer);
-        } catch (IOException e) {
-            logger.error("Error reading file", e);
-        }
-    };
-
     private static final CsvReader sequentialReader = (filepath, consumer) -> {
         try (Stream<String> reader = Files.lines(filepath) ){
             reader.skip(SKIP_CSV_LINES).forEach(consumer);
