69f39be3ba249fa1239e8fdab45b53fa5b07848c
Martin Hecht <mhecht@itba.edu.ar>
2024-06-13 13:30:56 -0300

    fix: query 2 collator small error handling

--- api/src/main/java/ar/edu/itba/pod/query2/TopInfractionsCollator.java
+++ api/src/main/java/ar/edu/itba/pod/query2/TopInfractionsCollator.java
@@ -1,13 +1,10 @@
 package ar.edu.itba.pod.query2;
 
-
 import ar.edu.itba.pod.Constants;
 import ar.edu.itba.pod.models.abstractClasses.Infraction;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 import com.hazelcast.mapreduce.Collator;
-
-import java.util.AbstractMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,15 +19,19 @@ public class TopInfractionsCollator implements Collator<Map.Entry<String, List<S
 
     @Override
     public Map<String, List<String>> collate(Iterable<Map.Entry<String, List<String>>> values) {
-        // map sorted by keys
+
+        // Creamos un mapa ordenado por key value (County alfabetico crecientemente)
         Map<String, List<String>> map = new java.util.TreeMap<>();
+
+        // Iteramos sobre los codigos de infracciones y obtenemos sus descripciones
+        // En caso de ver el codigo "-" repetimos dicho valor
         for (Map.Entry<String, List<String>> entry : values) {
             List<String> topInfractions = entry.getValue();
             //List<String> topInfractionsDescription = topInfractions.stream().filter(i -> !i.equals("-")).map(infractionCode -> infractions.get(infractionCode).getDescription()).toList();
 
             List<String> topInfractionsDescription = new java.util.ArrayList<>();
             for (String infractionCode : topInfractions) {
-                if (!infractionCode.equals("-")) {
+                if (!infractionCode.equals("-") || !infractions.containsKey(infractionCode)){
                     topInfractionsDescription.add(infractions.get(infractionCode).getDescription());
                 }else{
                     topInfractionsDescription.add("-");
